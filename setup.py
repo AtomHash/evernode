95b1ad511e64b86bdf96d4c28dae2808832484fd
Dylan Harty <me@dylanharty.com>
2017-09-22 09:02:02 -0600

    PyPi ready

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'evernode',
   packages = ['evernode'],
@@ -7,7 +7,17 @@ setup(
   author = 'Dylan Harty',
   author_email = 'me@dylanharty.com',
   url = 'https://github.com/dylanharty/evernode',
-  download_url = 'https://github.com/dylanharty/evernode/archive/0.1.tar.gz',
+  download_url = 'https://github.com/atomhash/evernode/archive/0.1.tar.gz',
   keywords = ['server', 'flask-based', 'restful', 'modular', 'evernode'],
+  install_requires=[
+    'flask',
+    'Flask-Cors==3.0.3',
+    'mysqlclient',
+    'Flask-SQLAlchemy==2.2',
+    'PyJWT',
+    'dill',
+    'cryptography',
+    'requests'
+  ],
   classifiers = [],
 )
