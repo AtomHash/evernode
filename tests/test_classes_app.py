c2c7b6f0a578eed9687931fa61d68881bc3ee508
Dylan Harty <me@dylanharty.com>
2018-04-17 15:49:49 -0600

    updated unit tests

--- tests/test_classes_app.py
+++ tests/test_classes_app.py
@@ -5,9 +5,19 @@ from flask import Flask
 
 
 class Test_Classes_App(unittest.TestCase):
+
+    app_class = None
+
+    def setUp(self):
+        with open('root_path.txt', 'r') as opened_file:
+            root_path = opened_file.read().replace('\n', '')
+            self.app_class = App(__name__, root_path=(root_path))
+
+    def tearDown(self):
+        self.app_class = None
+
     def test_app_is_flask(self):
-        app_class = App(__name__)
-        self.assertIsInstance(app_class.app, Flask)
+        self.assertIsInstance(self.app_class.app, Flask)
 
 
 if __name__ == '__main__':
--- tests/test_classes_security.py
+++ tests/test_classes_security.py
@@ -10,10 +10,9 @@ class Test_Classes_Security(unittest.TestCase):
     app_class = None
 
     def setUp(self):
-        self.app_class = App(
-            __name__,
-            root_path=('C:\\Users\\dylan.harty\\'
-                       'Desktop\\evernode\\app\\app\\app'))
+        with open('root_path.txt', 'r') as opened_file:
+            root_path = opened_file.read().replace('\n', '')
+            self.app_class = App(__name__, root_path=(root_path))
 
     def tearDown(self):
         self.app_class = None
