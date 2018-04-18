0e19609535986f3336067e2d1a9aba5aab078d3b
Dylan Harty <me@dylanharty.com>
2018-04-18 10:29:46 -0600

    update to travis script. added evernode app to test json

--- tests/test_classes_json.py
+++ tests/test_classes_json.py
@@ -1,18 +1,33 @@
 # pylint: skip-file
 
 import unittest
+import os
 import datetime
 from json.decoder import JSONDecodeError
 from evernode.classes import Json
+from evernode.classes import App
 
 
 class Test_Classes_Json(unittest.TestCase):
+
+    app_class = None
+
+    def setUp(self):
+        script_path = os.path.dirname(__file__)
+        root_path_file = os.path.join(script_path, 'root_path.txt')
+        with open(root_path_file, 'r') as opened_file:
+            root_path = opened_file.read().replace('\n', '')
+            self.app_class = App(__name__, root_path=(root_path))
+
+    def tearDown(self):
+        self.app_class = None
+
     def test_json_read_not_found(self):
         self.assertRaises(FileNotFoundError, Json.from_file, 'nofile')
 
     def test_json_read_found(self):
         self.assertIsInstance(
-            Json.from_file('../app/app/app/config.json'), dict)
+            Json.from_file(self.app_class.app.config['CONFIG_PATH']), dict)
 
     def test_json_string_parse(self):
         self.assertIsInstance(
