a475f6ce440fb364de302e9514c4051c4a1707fb
Dylan Harty <me@dylanharty.com>
2018-04-29 22:07:16 -0600

    added docker support to create.py, testing fix for travis

--- evernode/bin/create.py
+++ evernode/bin/create.py
@@ -15,9 +15,11 @@ class Create:
     uwsgi_file = None
     app_file = None
     http_messages_file = None
+    branch = None
 
-    def __init__(self, dir_name):
+    def __init__(self, dir_name, branch='dev-1.1.0'):
         self.dir_name = './evernode_%s' % (dir_name)
+        self.branch = branch
         self.app_file = os.path.join(self.dir_name, 'app', 'app.py')
         self.http_messages_file = os.path.join(
             self.dir_name, 'app', 'resources',
@@ -49,7 +51,7 @@ class Create:
         # download current config file from github
         self.download_file(
             ('https://raw.githubusercontent.com/AtomHash/evernode/'
-                'master/app/app/app/config.json'),
+                '%s/app/app/config.json' % (self.branch)),
             self.config_file)
         config = Json.from_file(self.config_file)
         config['SERECT'] = Security.generate_key()
@@ -59,23 +61,71 @@ class Create:
     def download_sample_uwsgi(self):
         self.download_file(
             ('https://raw.githubusercontent.com/AtomHash/evernode/'
-                'master/app/app/uwsgi.ini'),
+                '%s/app/uwsgi.ini' % (self.branch)),
             self.uwsgi_file)
 
     def download_sample_app(self):
         self.download_file(
             ('https://raw.githubusercontent.com/AtomHash/evernode/'
-                'master/app/app/app/app.py'),
+                '%s/app/app/app.py' % (self.branch)),
             self.app_file)
 
     def download_sample_http_errors(self):
         self.download_file(
             ('https://raw.githubusercontent.com/AtomHash/evernode/'
-                'master/app/app/app/resources/lang/en/http_messages.lang'),
+                '%s/app/app/resources/lang/en/http_messages.lang'
+                % (self.branch)),
             self.http_messages_file)
 
     def configure_docker(self):
-        pass
+        needed_files = [
+            {'docker': [
+                ('https://raw.githubusercontent.com/AtomHash/evernode/'
+                 '%s/app/docker/docker-compose.yml' % (self.branch))
+            ]},
+            {'build': [
+                ('https://raw.githubusercontent.com/AtomHash/evernode/'
+                 '%s/app/docker/build/supervisord.conf' % (self.branch)),
+                ('https://raw.githubusercontent.com/AtomHash/evernode/'
+                 '%s/app/docker/build/Dockerfile' % (self.branch)),
+            ]},
+            {'nginx': [
+                ('https://raw.githubusercontent.com/AtomHash/evernode/'
+                 '%s/app/docker/build/nginx/nginx.conf' % (self.branch))
+            ]},
+            {'ssls': [
+                ('https://raw.githubusercontent.com/AtomHash/evernode/'
+                 '%s/app/docker/build/nginx/ssls/'
+                 'api.localhost.crt' % (self.branch)),
+                ('https://raw.githubusercontent.com/AtomHash/evernode/'
+                 '%s/app/docker/build/nginx/ssls/'
+                 'api.localhost.key' % (self.branch))
+            ]},
+            {'conf.d': [
+                ('https://raw.githubusercontent.com/AtomHash/evernode/'
+                 '%s/app/docker/build/nginx/conf.d/'
+                 'api.localhost.conf' % (self.branch))
+            ]}]
+        for folder in needed_files:
+            for key, value in folder.items():
+                    root = ''
+                    if key is 'docker':
+                        root = 'docker'
+                    elif key is 'build':
+                        root = 'docker/build'
+                    elif key is 'nginx':
+                        root = 'docker/build/nginx'
+                    elif key is 'ssls':
+                        root = 'docker/build/nginx/ssls'
+                    elif key is 'conf.d':
+                        root = 'docker/build/nginx/conf.d'
+                    os.mkdir(os.path.join(self.dir_name, root))
+                    for file in value:
+                        self.__docker_file_download(root, file)
+
+    def __docker_file_download(self, root, file):
+        self.download_file(file, os.path.join(
+            self.dir_name, root, file.rsplit('/', 1)[-1]))
 
     def make_structure(self):
         if os.path.isdir(self.dir_name):
