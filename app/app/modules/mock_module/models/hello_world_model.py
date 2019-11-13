d70ca26c22d50f1bf840f224f4d751402ee80483
Dylan Harty <me@dylanharty.com>
2019-11-13 13:30:20 -0700

    added back mock_module for evernode console app

---
+++ app/app/modules/mock_module/models/hello_world_model.py
@@ -0,0 +1,12 @@
+"""
+    Hello World Model
+"""
+
+# from evernode.models import BaseUserModel
+
+
+class HelloWorldModel:
+    """ Mock Module, Mock Model """
+
+    def __init__(self):
+        self.hello = 'world'
